90b0386c40d28b84658082ac9a0375abd57f8997
user <user@FE07>
2020-03-17 11:25:16 +0800

    hw1

---
+++ src/fcu/iecs/oop/Hello.java
@@ -0,0 +1,10 @@
+package fcu.iecs.oop;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String lyrics = "Let it go! Let it go! Cannot hold it back anymore";
+	}
+
+}
