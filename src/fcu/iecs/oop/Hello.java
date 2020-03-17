1c501cfd1391bd072b7471d1eab77513aff9c289
user <user@FE07>
2020-03-17 11:37:41 +0800

    修改

--- src/fcu/iecs/oop/Hello.java
+++ src/fcu/iecs/oop/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String lyrics = "Let it go! Let it go! Cannot hold it back anymore";
+		String lyrics = "Let her go! Let her go! Can't hold her back anymore";
 		System.out.println(lyrics);
 	}
 
